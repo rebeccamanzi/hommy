d685a0c86379c8f25a73fdbdbc2a7fb9f6318026
Rebecca Manzi <rebeccamanzi@gmail.com>
2020-05-10 19:17:26 -0300

    integracao com api

---
+++ assets/scripts/index.js
@@ -0,0 +1,44 @@
+const apiUrl = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
+const cardsContainer = document.querySelector("#cards");
+let data = [];
+
+//retornar json da api com o fetch
+async function fetchCards() {
+    const response = await fetch(apiUrl);
+    return await response.json();
+}
+
+function renderCards(cards) {
+    cardsContainer.innerHTML = "";
+    cards.map(renderCard);
+}
+
+function renderCard(card) {
+    const div = document.createElement("div");
+    div.style.width = "20rem";
+    div.style.margin = "2rem";
+    div.className = "card";
+    div.innerHTML = `
+        <img src="${card.photo}" class="card-img-top" alt="${card.name}" />
+        <div class="card-body">
+            <h5 class="card-title">${card.name}</h5>
+            <p class="card-text">
+                Tipo: ${card.property_type}
+            </p>
+            <p class="card-text">
+                Preço: ${card.price},00
+            </p>
+        </div>
+    `;
+    cardsContainer.appendChild(div);
+}
+
+
+async function main() {
+    data = await fetchCards();
+    if (data[0]) {
+        renderCards(data);
+    }
+}
+
+main();
